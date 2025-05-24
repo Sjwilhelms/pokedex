71c638641c8781dde5a31248ed8b2b6fcd444f76
Sam <sjwilliams388@gmail.com>
2025-05-24 14:08:53 +0100

    access more info on pokemon

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
+
 fetchData();
 
 async function fetchData(){
     try{
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/typhlosion");
+
+        const pokemonName = document.getElementById("pokemonName").value;
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok){
             throw new error("Could not fetch resource");
@@ -11,7 +15,25 @@ async function fetchData(){
         const data = await response.json();
         console.log(data.name);
         console.log(data.id);
-        console.log(data.sprite);
+        
+        const pokemonSprite = data.sprites.front_default;
+        const pokemonType = data.types[0].type.name;
+        const pokemonHeight = data.height;
+        const pokemonWeight = data.weight;
+
+        const imgElement = document.getElementById("pokemonSprite");
+        const typeElement = document.getElementById("pokemonType");
+        const heightElement = document.getElementById("pokemonHeight");
+        const weightElement = document.getElementById("pokemonWeight");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+        typeElement.innerText = pokemonType;
+        typeElement.style.display = "block";
+        heightElement.innerText = pokemonHeight;
+        heightElement.style.display = "block";
+        weightElement.innerText = pokemonWeight;
+        weightElement.style.display = "block";
     }
 
     catch(error){
